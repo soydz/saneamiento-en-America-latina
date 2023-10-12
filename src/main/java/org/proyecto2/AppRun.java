36aeb61f13b970b542c9afed919c7695f127a9ad
soydz <soydz@pm.me>
2023-10-12 18:50:44 -0500

    Se ejecutan los metodos definidos en Service

--- src/main/java/org/proyecto2/AppRun.java
+++ src/main/java/org/proyecto2/AppRun.java
@@ -1,7 +1,16 @@
 package org.proyecto2;
 
+import org.proyecto2.repository.CountrySanitationStatisticsRepositoryImpl;
+import org.proyecto2.service.SanitationStatisticService;
+import org.proyecto2.service.SanitationStatisticServiceImpl;
 public class AppRun {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        SanitationStatisticService sanitationStatisticService =
+                new SanitationStatisticServiceImpl(new CountrySanitationStatisticsRepositoryImpl());
+
+        double averageWaterAccess = sanitationStatisticService.averageWaterAccess();
+        String countryGreaterAccessWater = sanitationStatisticService.countryGreaterAccessWater();
+
     }
 }
