cc6aaf6a70882a985f903484aced6608c33dc26a
soydz <soydz@pm.me>
2023-10-13 18:49:55 -0500

    Se comentan metodos, que posiblemente seran eliminados

--- src/main/java/org/proyecto2/service/SanitationStatisticService.java
+++ src/main/java/org/proyecto2/service/SanitationStatisticService.java
@@ -5,7 +5,13 @@ import java.util.ArrayList;
 public interface SanitationStatisticService {
     Double averageWaterAccess();
     String countryGreaterAccessWater();
+
+    //String countryLessAccessWater();
+    //String countryMoreHospitalsPerHundredThousandPeople();
+    //Double percentageHospitalsSanitationProblems();
+
     Double medianPopulation();
+
     Double averagePopulationAffectedByDrought();
     Double varianceAffectedPopulationDroughts(Double averagePopulationAffectedByDrought);
     ArrayList<Integer> sortData(ArrayList<Integer> list);
