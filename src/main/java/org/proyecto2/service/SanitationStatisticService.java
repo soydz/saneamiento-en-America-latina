1b962a8a5ae8ca12a1576020a7bf5c5cb20a73af
soydz <soydz@pm.me>
2023-10-13 09:56:55 -0500

    Se definen los metodos

--- src/main/java/org/proyecto2/service/SanitationStatisticService.java
+++ src/main/java/org/proyecto2/service/SanitationStatisticService.java
@@ -1,8 +1,12 @@
 package org.proyecto2.service;
 
+import java.util.ArrayList;
+
 public interface SanitationStatisticService {
     Double averageWaterAccess();
     String countryGreaterAccessWater();
-
-
+    Double medianPopulation();
+    Double averagePopulationAffectedByDrought();
+    Double varianceAffectedPopulationDroughts(Double averagePopulationAffectedByDrought);
+    ArrayList<Integer> sortData(ArrayList<Integer> list);
 }
