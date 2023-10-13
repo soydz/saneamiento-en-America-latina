e2435ccd1520898cbcf343864022761c402df2a0
soydz <soydz@pm.me>
2023-10-13 10:01:26 -0500

    Se implementan los metodos de la interfaz

--- src/main/java/org/proyecto2/service/SanitationStatisticServiceImpl.java
+++ src/main/java/org/proyecto2/service/SanitationStatisticServiceImpl.java
@@ -1,6 +1,8 @@
 package org.proyecto2.service;
 
+import java.util.ArrayList;
 import java.util.List;
+
 import org.proyecto2.model.CountrySanitationStatistics;
 import org.proyecto2.repository.CountrySanitationStatisticsRepository;
 
@@ -43,5 +45,73 @@ public class SanitationStatisticServiceImpl implements SanitationStatisticServic
         }
 
         return country;
+    }
+
+    @Override
+    public Double medianPopulation(){
+        List<CountrySanitationStatistics> countriesList = this.countrySanitationStatisticsRepository.statisticsOfCountries();
+        ArrayList<Integer> population = new ArrayList<Integer>();
+
+        for(CountrySanitationStatistics i:countriesList){
+            population.add(i.population());
+        }
+
+        ArrayList<Integer> orderedPopulation = (ArrayList<Integer>) sortData(population).clone();
+
+        if((orderedPopulation.size() % 2) == 0){
+            int data = orderedPopulation.size() / 2;
+
+            return Double.valueOf( (orderedPopulation.get(data - 1) + orderedPopulation.get(data)) / 2 );
+        }else {
+            int data = (int) Math.floor(orderedPopulation.size() / 2);
+
+            return Double.valueOf( orderedPopulation.get(data) );
+        }
+    }
+
+    @Override
+    public Double averagePopulationAffectedByDrought(){
+        List<CountrySanitationStatistics> countriesList = this.countrySanitationStatisticsRepository.statisticsOfCountries();
+
+        Double sum = 0D;
+
+        for(CountrySanitationStatistics i:countriesList){
+            sum += i.populationAffectedByDrought();
+        }
+
+        return sum / countriesList.size();
+    }
+
+    @Override
+    public Double varianceAffectedPopulationDroughts(Double averagePopulationAffectedByDrought){
+        List<CountrySanitationStatistics> countriesList = this.countrySanitationStatisticsRepository.statisticsOfCountries();
+        Double sumSquare = 0D;
+
+        for(CountrySanitationStatistics i: countriesList){
+            Double difference = i.populationAffectedByDrought() - averagePopulationAffectedByDrought;
+            sumSquare += difference * difference;
+        }
+
+        return sumSquare / countriesList.size();
+    }
+
+
+    @Override
+    public ArrayList<Integer> sortData(ArrayList<Integer> list){
+        ArrayList<Integer> orderedList = list;
+
+        for(int i = 1; i < list.size(); i++){
+            for(int j = 0; j < (list.size() - i); j++ ){
+
+                if(orderedList.get(j) > orderedList.get(j + 1)){
+                    Integer num = orderedList.get(j + 1);
+                    orderedList.set((j + 1), orderedList.get(j));
+                    orderedList.set(j, num);
+                }
+
+            }
+        }
+
+        return orderedList;
     }
 }
