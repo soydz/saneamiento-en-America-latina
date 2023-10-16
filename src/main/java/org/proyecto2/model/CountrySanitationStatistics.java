31ecc93fbcc5b62dc6b638a14e3e70d060b89505
soydz <soydz@pm.me>
2023-10-16 18:16:49 -0500

    Se reformatea el codigo para que siga la misma estructura de todo el proyecto

--- src/main/java/org/proyecto2/model/CountrySanitationStatistics.java
+++ src/main/java/org/proyecto2/model/CountrySanitationStatistics.java
@@ -1,9 +1,10 @@
 package org.proyecto2.model;
 
-public record CountrySanitationStatistics(String country,
-                                          Integer population,
-                                          Double waterAccessPercentage,
-                                          Integer populationAffectedByDrought,
-                                          Integer numberOfHospitals,
-                                          Integer numberOfHospitalsSanitationProblems,
-                                          Double numberOfWaterTreatmentPlantsPerHundredThousandInhabitants) {}
+public record CountrySanitationStatistics(
+        String country,
+        Integer population,
+        Double waterAccessPercentage,
+        Integer populationAffectedByDrought,
+        Integer numberOfHospitals,
+        Integer numberOfHospitalsSanitationProblems,
+        Double numberOfWaterTreatmentPlantsPerHundredThousandInhabitants) {}
