4751f4950c817ff120e989d35a878d46d7abb46e
soydz <soydz@pm.me>
2023-10-16 16:26:41 -0500

    Se refactoriza el codigo

--- src/main/java/org/proyecto2/service/SanitationStatisticServiceImpl.java
+++ src/main/java/org/proyecto2/service/SanitationStatisticServiceImpl.java
@@ -6,23 +6,23 @@ import java.util.List;
 import org.proyecto2.model.CountrySanitationStatistics;
 import org.proyecto2.repository.CountrySanitationStatisticsRepository;
 
-public class SanitationStatisticServiceImpl implements SanitationStatisticService{
+public class SanitationStatisticServiceImpl implements SanitationStatisticService {
 
     private final CountrySanitationStatisticsRepository countrySanitationStatisticsRepository;
+    private final List<CountrySanitationStatistics> countriesList;
 
-    public SanitationStatisticServiceImpl(CountrySanitationStatisticsRepository countrySanitationStatisticsRepository){
+    public SanitationStatisticServiceImpl(
+            CountrySanitationStatisticsRepository countrySanitationStatisticsRepository) {
         this.countrySanitationStatisticsRepository = countrySanitationStatisticsRepository;
+        this.countriesList = this.countrySanitationStatisticsRepository.statisticsOfCountries();
     }
 
-
     @Override
-    public Double averageWaterAccess(){
-        List<CountrySanitationStatistics> countriesList = this.countrySanitationStatisticsRepository.statisticsOfCountries();
-
+    public Double averageWaterAccess() {
         Double sumAverageWaterAccess = 0D;
-        int numCountries = countriesList.size();
+        int numCountries = this.countriesList.size();
 
-        for(CountrySanitationStatistics i:countriesList){
+        for (CountrySanitationStatistics i : this.countriesList) {
             sumAverageWaterAccess += i.waterAccessPercentage();
         }
 
@@ -30,15 +30,13 @@ public class SanitationStatisticServiceImpl implements SanitationStatisticServic
     }
 
     @Override
-    public String countryGreaterAccessWater(){
-        List<CountrySanitationStatistics> countriesList = this.countrySanitationStatisticsRepository.statisticsOfCountries();
-
+    public String countryGreaterAccessWater() {
         String country = "";
         Double waterAccessPercentage = 0D;
 
-        for(CountrySanitationStatistics i:countriesList){
+        for (CountrySanitationStatistics i : this.countriesList) {
 
-            if(i.waterAccessPercentage() > waterAccessPercentage){
+            if (i.waterAccessPercentage() > waterAccessPercentage) {
                 country = i.country();
                 waterAccessPercentage = i.waterAccessPercentage();
             }
@@ -48,67 +46,63 @@ public class SanitationStatisticServiceImpl implements SanitationStatisticServic
     }
 
     @Override
-    public Double medianPopulation(){
-        List<CountrySanitationStatistics> countriesList = this.countrySanitationStatisticsRepository.statisticsOfCountries();
+    public Double medianPopulation() {
         ArrayList<Integer> population = new ArrayList<Integer>();
 
-        for(CountrySanitationStatistics i:countriesList){
+        for (CountrySanitationStatistics i : this.countriesList) {
             population.add(i.population());
         }
 
         ArrayList<Integer> orderedPopulation = (ArrayList<Integer>) sortData(population).clone();
 
-        if((orderedPopulation.size() % 2) == 0){
+        if ((orderedPopulation.size() % 2) == 0) {
             int data = orderedPopulation.size() / 2;
 
-            return Double.valueOf( (orderedPopulation.get(data - 1) + orderedPopulation.get(data)) / 2 );
-        }else {
+            return Double.valueOf(
+                    (orderedPopulation.get(data - 1) + orderedPopulation.get(data)) / 2);
+        } else {
             int data = (int) Math.floor(orderedPopulation.size() / 2);
 
-            return Double.valueOf( orderedPopulation.get(data) );
+            return Double.valueOf(orderedPopulation.get(data));
         }
     }
 
     @Override
-    public Double averagePopulationAffectedByDrought(){
-        List<CountrySanitationStatistics> countriesList = this.countrySanitationStatisticsRepository.statisticsOfCountries();
-
+    public Double averagePopulationAffectedByDrought() {
         Double sum = 0D;
 
-        for(CountrySanitationStatistics i:countriesList){
+        for (CountrySanitationStatistics i : this.countriesList) {
             sum += i.populationAffectedByDrought();
         }
 
-        return sum / countriesList.size();
+        return sum / this.countriesList.size();
     }
 
     @Override
-    public Double varianceAffectedPopulationDroughts(Double averagePopulationAffectedByDrought){
-        List<CountrySanitationStatistics> countriesList = this.countrySanitationStatisticsRepository.statisticsOfCountries();
+    public Double varianceAffectedPopulationDroughts(Double averagePopulationAffectedByDrought) {
         Double sumSquare = 0D;
 
-        for(CountrySanitationStatistics i: countriesList){
-            Double difference = i.populationAffectedByDrought() - averagePopulationAffectedByDrought;
+        for (CountrySanitationStatistics i : this.countriesList) {
+            Double difference =
+                    i.populationAffectedByDrought() - averagePopulationAffectedByDrought;
             sumSquare += difference * difference;
         }
 
-        return sumSquare / countriesList.size();
+        return sumSquare / this.countriesList.size();
     }
 
-
     @Override
-    public ArrayList<Integer> sortData(ArrayList<Integer> list){
+    public ArrayList<Integer> sortData(ArrayList<Integer> list) {
         ArrayList<Integer> orderedList = list;
 
-        for(int i = 1; i < list.size(); i++){
-            for(int j = 0; j < (list.size() - i); j++ ){
+        for (int i = 1; i < list.size(); i++) {
+            for (int j = 0; j < (list.size() - i); j++) {
 
-                if(orderedList.get(j) > orderedList.get(j + 1)){
+                if (orderedList.get(j) > orderedList.get(j + 1)) {
                     Integer num = orderedList.get(j + 1);
                     orderedList.set((j + 1), orderedList.get(j));
                     orderedList.set(j, num);
                 }
-
             }
         }
 
