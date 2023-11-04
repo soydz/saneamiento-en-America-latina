fa78e1d66391fdc0ac0e060dbe8fbbc4c3e04259
soydz <soydz@pm.me>
2023-11-03 20:23:19 -0500

    Refactorización de codigo usando programación funcional

--- src/main/java/org/saneamientoAmericaLatina/service/SanitationStatisticServiceImpl.java
+++ src/main/java/org/saneamientoAmericaLatina/service/SanitationStatisticServiceImpl.java
@@ -1,6 +1,5 @@
 package org.saneamientoAmericaLatina.service;
 
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
@@ -34,15 +33,13 @@ public class SanitationStatisticServiceImpl implements SanitationStatisticServic
 
     @Override
     public Double medianPopulation() {
-        ArrayList<Integer> population = new ArrayList<>();
 
-        for (CountrySanitationStatistics i : this.countriesList) {
-            if (i.population() != null) {
-                population.add(i.population());
-            }
-        }
-
-        ArrayList<Integer> orderedPopulation = (ArrayList<Integer>) sortData(population).clone();
+        List<Integer> orderedPopulation =
+                this.countriesList.stream()
+                        .mapToInt(CountrySanitationStatistics::population)
+                        .sorted()
+                        .boxed()
+                        .toList();
 
         if ((orderedPopulation.size() % 2) == 0) {
             int data = orderedPopulation.size() / 2;
@@ -79,22 +76,5 @@ public class SanitationStatisticServiceImpl implements SanitationStatisticServic
         }
 
         return sumSquare / numCountries;
-    }
-
-    @Override
-    public ArrayList<Integer> sortData(ArrayList<Integer> list) {
-
-        for (int i = 1; i < list.size(); i++) {
-            for (int j = 0; j < (list.size() - i); j++) {
-
-                if (list.get(j) > list.get(j + 1)) {
-                    Integer num = list.get(j + 1);
-                    list.set((j + 1), list.get(j));
-                    list.set(j, num);
-                }
-            }
-        }
-
-        return list;
     }
 }
